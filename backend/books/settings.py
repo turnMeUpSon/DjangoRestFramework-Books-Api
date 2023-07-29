8dc4d3380f1c0656e8cd9b020a203be9386b9b0e
Ian Miller <ianmiller@Ians-MacBook-Pro.local>
2023-07-29 18:24:58 +0600

    Created OAuth with github

--- backend/books/settings.py
+++ backend/books/settings.py
@@ -4,10 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ru_$2cxq9k@v(_jx2d=-6ua_%0ej2rsn3x5tbhhr%rsu2cp0e='
 
